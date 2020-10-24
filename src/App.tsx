7722f325e5a7a1bdb4d629b4ab1eabd45da22841
hota1024 <hotalog@hota1024.com>
2020-10-24 20:04:25 +0900

    🏷️ Add random cat system

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
 import * as React from 'react'
+import axios from 'axios'
 
 export const App = (): React.ReactElement => {
-  return <div>Hello, Figma Plugin with React</div>
+  const [imgSrc, setImgSrc] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+
+  const getRandomCat = async () => {
+    setLoading(true)
+
+    try {
+      const res = await axios.get(
+        'https://source.unsplash.com/random/800x600?cat'
+      )
+
+      const url: string = res.request.responseURL
+
+      setImgSrc(url)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={getRandomCat} disabled={loading}>
+        {loading ? 'Loading...' : 'Random Cat'}
+      </button>
+      <br />
+      {imgSrc && <img src={imgSrc} width="100%" />}
+    </div>
+  )
 }
